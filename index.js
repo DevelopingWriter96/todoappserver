a1f63da000c492299c781413125f0b3d3dda94df
Ammon Roberts <ammoneroberts@gmail.com>
2021-09-22 17:25:04 -0600

    Added completeTodo, refactored some stuff
    Co-authored-by: Tanner Fronk <tannerfronk@users.noreply.github.com>
    Co-authored-by: DevelopingWriter96 <DevelopingWriter96@users.noreply.github.com>
    Co-authored-by: Ry-Hebert <Ry-Hebert@users.noreply.github.com>
    Co-authored-by: moe1129peterson <moe1129peterson@users.noreply.github.com>
    Co-authored-by: Daniela Sion  <danielacsion@users.noreply.github.com>

--- index.js
+++ index.js
@@ -2,38 +2,40 @@ let todos = [
     {
         id: 1,
         taskName: 'Task 1',
-        completed: 'Incomplete',
+        completed: false,
     },
     {
         id: 2,
         taskName: 'Task 2',
-        completed: 'Incomplete',
+        completed: false,
     },
     {
         id: 3,
         taskName: 'Task 3',
-        completed: 'Incomplete',
+        completed: false,
     },
 ]
 
 const updateTodos = () => {
-    let root = document.querySelector('#root')
-    root.innerHTML = `<ul>`
+    let taskList = document.querySelector('#taskList')
+    taskList.innerHTML = `<ul>`
 
     todos.map((todo) => {
-        root.innerHTML +=
+        taskList.innerHTML +=
             `<li class="card">
                 <div class="card-body">
-                    <input type="checkbox" id="task${todo.id}" name="task${todo.id}" value="Bike">
+                    <button type="button" class="btn-close float-end" aria-label="Close" onclick="deleteTodo(${todo.id})"></button>
+                    <input type="checkbox" id="task${todo.id}" name="task${todo.id}" onclick="completeTodo(${todo.id})">
                     <label for="task${todo.id}"> ${todo.taskName}</label><br>
                 </div>
             </li>`
     })
-    root.innerHTML += `</ul>`
+    taskList.innerHTML += `</ul>`
 }
 
-updateTodos();
-document.querySelector("#addTaskButton").addEventListener('click', (event) => {
+
+
+document.querySelector("#addTaskButton").addEventListener('click', () => {
     let inputBox = document.querySelector('#newTaskName');
     let newTaskName = inputBox.value
     let newId = todos.length + 1
@@ -43,3 +45,27 @@ document.querySelector("#addTaskButton").addEventListener('click', (event) => {
     todos.push({ id: newId, taskName: newTaskName, completed: false })
     updateTodos();
 })
+
+updateTodos();
+
+
+function deleteTodo(id) {
+    todos = todos.filter(element => {
+        if (element.id != id) {
+            return element;
+        }
+
+    })
+
+    updateTodos()
+}
+
+function completeTodo(id) {
+    todos = todos.filter(element => {
+        if (element.id == id) {
+            element.completed = !element.completed;
+        }
+        return element;
+    })
+}
+
