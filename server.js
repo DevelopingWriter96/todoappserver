1299e56fdf34a910e0c5f273b55adec771fb12d6
DevelopingWriter96 <staceyrichard1996@gmail.com>
2021-11-07 19:01:00 -0700

    Server functional

--- server.js
+++ server.js
@@ -1,3 +1,18 @@
 const mongoose = require('mongoose');
+const {mongoPass, mongoUser} = require('./config')
+
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => {
+    console.log('Connected Successfully')
+})
+.catch(console.error)
+
+// require('./createUser')
+// require('./getUsers')
+// require('./updateUser')
+// require('./deleteUser')
 
 
